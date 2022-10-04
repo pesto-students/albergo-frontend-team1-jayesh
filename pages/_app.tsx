97554744985df45548dead1ed2cd3b4e3766de7f
Siddhant-ops <siddhantdalvi3@gmail.com>
2022-10-04 22:12:05 +0530

    partner-homepage added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
       <Newsletter />
       <Footer />
-      <AuthModal />
+      {/* <AuthModal /> */}
     </Fragment>
   );
 }
