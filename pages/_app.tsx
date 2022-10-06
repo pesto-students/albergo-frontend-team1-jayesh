0951f6cdab9f6cf6d4237f87d3d42be87dc93624
Siddhant-ops <siddhantdalvi3@gmail.com>
2022-10-06 19:12:35 +0530

    redux toolkit setup done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,22 +6,29 @@ import Footer from '../Components/Footer/Footer';
 import Newsletter from '../Components/Newsletter/Newsletter';
 import { Fragment } from 'react';
 import { AuthModalProvider } from '../context/providers/AuthModal.provider';
-import AuthModal from '../Components/AuthModal/AuthModal';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+// import AuthModal from '../Components/AuthModal/AuthModal';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <AuthModalProvider>
+      <Provider store={store}>
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
+        {/* <AuthModalProvider> */}
         <Navbar />
-        <AuthModal />
-      </AuthModalProvider>
-      <Component {...pageProps} />
-      <Newsletter />
-      <Footer />
+        {/* <AuthModal /> */}
+        {/* </AuthModalProvider> */}
+        <Component {...pageProps} />
+        <Newsletter />
+        <Footer />
+      </Provider>
     </Fragment>
   );
 }
