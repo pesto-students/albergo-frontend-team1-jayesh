10e192b653c94059ff603784330ec2a395c2303d
Siddhant-ops <siddhantdalvi3@gmail.com>
2022-10-09 11:14:16 +0530

    dependencies updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,12 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Fragment } from 'react';
+import { Provider } from 'react-redux';
 import '../styles/globals.scss';
 import Navbar from '../Components/Navbar/Navbar';
 import Footer from '../Components/Footer/Footer';
 import Newsletter from '../Components/Newsletter/Newsletter';
-import { Fragment } from 'react';
-import { AuthModalProvider } from '../context/providers/AuthModal.provider';
-import { Provider } from 'react-redux';
 import store from '../redux/store';
-// import AuthModal from '../Components/AuthModal/AuthModal';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -21,10 +19,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             content="width=device-width, initial-scale=1.0"
           />
         </Head>
-        {/* <AuthModalProvider> */}
         <Navbar />
-        {/* <AuthModal /> */}
-        {/* </AuthModalProvider> */}
         <Component {...pageProps} />
         <Newsletter />
         <Footer />
