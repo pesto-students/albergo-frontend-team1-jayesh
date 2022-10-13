35f00f765c1e492260d104f9cf171e4f9379c77d
Siddhant-ops <siddhantdalvi3@gmail.com>
2022-10-13 23:38:43 +0530

    login-signup auth token setup complete

--- Utils/auth/signup.ts
+++ Utils/auth/signup.ts
@@ -1,13 +1,27 @@
-import { checkPassword, isValidEmail, isValidPassword } from './authHelper';
+import { Dispatch, SetStateAction } from 'react';
+import { IToast } from '../../Components/Toast/Toast';
+import store from '../../redux/store';
+import { setUserEncryptedToken } from '../../redux/user/user.slice';
+import {
+  // checkPassword,
+  // isValidateName,
+  // isValidEmail,
+  // isValidPassword,
+  setTokenCookie
+} from './authHelper';
 
 interface IAuthSignup {
+  name: string;
   email: string;
   password: string;
   confirmPassword: string;
 }
 
-const signupPost = async (formObj: IAuthSignup) => {
-  const { email, password } = formObj;
+const signupPost = async (
+  formObj: IAuthSignup,
+  setToastState: Dispatch<SetStateAction<IToast>>
+) => {
+  const { name, email, password } = formObj;
 
   const response = await fetch('/api/auth/signup', {
     method: 'POST',
@@ -15,36 +29,69 @@ const signupPost = async (formObj: IAuthSignup) => {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
+      name,
       email,
       password
     })
   })
-    .then((response) => response.json())
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.error) throw new Error(res.error);
+
+      if (res?.data?.status === 'fail') {
+        setToastState({
+          message: res.data.message,
+          type: 'error',
+          visible: true
+        });
+      }
+
+      if (res?.data?.status === 'success') {
+        const token = res?.data?.token;
+        setTokenCookie(token);
+        store.dispatch(setUserEncryptedToken(token));
+        setToastState({
+          message: 'Signup successful',
+          type: 'success',
+          visible: true
+        });
+      }
+    })
     .catch((err) => {
       // error handling
-      console.log('error', err);
+      setToastState({
+        message: err.message,
+        type: 'error',
+        visible: true
+      });
     });
 
   console.log(response);
   return response;
 };
 
-const signupForm = async (formObj: IAuthSignup) => {
-  const { email, password, confirmPassword } = formObj;
+const signupForm = async (
+  formObj: IAuthSignup,
+  setToastState: Dispatch<SetStateAction<IToast>>
+) => {
+  // const { name, email, password, confirmPassword } = formObj;
 
-  // check if email and password are valid
-  if (
-    !email ||
-    !password ||
-    !isValidEmail(email) ||
-    !isValidPassword(password) ||
-    !checkPassword(password, confirmPassword)
-  ) {
-    console.error({ error: 'Invalid email or password' });
-  }
+  // // check if email and password are valid
+  // if (
+  //   !isValidateName(name) ||
+  //   !isValidEmail(email) ||
+  //   !isValidPassword(password) ||
+  //   !checkPassword(password, confirmPassword)
+  // ) {
+  //   setToastState({
+  //     message: 'Invalid credentials',
+  //     type: 'error',
+  //     visible: true
+  //   });
+  //   return;
+  // }
 
-  const response = await signupPost(formObj);
-  console.log(response);
+  await signupPost(formObj, setToastState);
 };
 
 export { signupForm };
