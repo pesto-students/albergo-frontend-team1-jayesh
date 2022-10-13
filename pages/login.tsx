66647b1fe332edbe6b59297a79a918c37a6dfdb7
Siddhant-ops <siddhantdalvi3@gmail.com>
2022-10-13 12:31:33 +0530

    toast component complete

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
-import { useRef } from 'react';
+import { Fragment, useRef, useState } from 'react';
 import LoginContainer from '../Components/Login/loginContainer';
+import Toast, { IToast } from '../Components/Toast/Toast';
 import loginStyles from '../styles/Login/login.module.scss';
 import { loginForm } from '../Utils/auth/login';
 
@@ -8,75 +9,83 @@ const Login = () => {
   const emailInput = useRef<HTMLInputElement>(null);
   const passwordInput = useRef<HTMLInputElement>(null);
 
+  const [toastState, setToastState] = useState<IToast>({
+    message: 'Hello Some error',
+    type: 'warning',
+    visible: true
+  });
+
+  const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const email = emailInput.current?.value;
+    const password = passwordInput.current?.value;
+    if (email && password) {
+      loginForm({ email, password });
+    }
+  };
+
   return (
-    <LoginContainer title="login">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (emailInput.current && passwordInput.current)
-            loginForm({
-              email: emailInput.current.value,
-              password: passwordInput.current.value
-            });
-        }}
-        className={loginStyles.form}
-      >
-        <div className={loginStyles.formGroup}>
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" ref={emailInput} />
-        </div>
-        <div className={loginStyles.formGroup}>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            ref={passwordInput}
-          />
-        </div>
-        <button type="submit">Login</button>
-        <div className={loginStyles.dividerWithText}>
-          <hr />
-          <small>or continue with</small>
-          <hr />
-        </div>
-        <div className={loginStyles.altAuthBtnContainer}>
-          <button>
-            <div className={loginStyles.icon}>
-              <Image
-                src="/assets/icons/fbIcon.png"
-                width={15}
-                height={15}
-                alt="icon"
-              />
-            </div>
-            facebook
-          </button>
-          <button>
-            <div className={loginStyles.icon}>
-              <Image
-                src="/assets/icons/googleIcon.png"
-                width={15}
-                height={15}
-                alt="icon"
-              />
-            </div>
-            google
-          </button>
-          <button>
-            <div className={loginStyles.icon}>
-              <Image
-                src="/assets/icons/appleIcon.png"
-                width={15}
-                height={15}
-                alt="icon"
-              />
-            </div>
-            apple
-          </button>
-        </div>
-      </form>
-    </LoginContainer>
+    <Fragment>
+      <LoginContainer title="login">
+        <form onSubmit={submitForm} className={loginStyles.form}>
+          <div className={loginStyles.formGroup}>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" ref={emailInput} />
+          </div>
+          <div className={loginStyles.formGroup}>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              ref={passwordInput}
+            />
+          </div>
+          <button type="submit">Login</button>
+          <div className={loginStyles.dividerWithText}>
+            <hr />
+            <small>or continue with</small>
+            <hr />
+          </div>
+          <div className={loginStyles.altAuthBtnContainer}>
+            <button>
+              <div className={loginStyles.icon}>
+                <Image
+                  src="/assets/icons/fbIcon.png"
+                  width={15}
+                  height={15}
+                  alt="icon"
+                />
+              </div>
+              facebook
+            </button>
+            <button>
+              <div className={loginStyles.icon}>
+                <Image
+                  src="/assets/icons/googleIcon.png"
+                  width={15}
+                  height={15}
+                  alt="icon"
+                />
+              </div>
+              google
+            </button>
+            <button>
+              <div className={loginStyles.icon}>
+                <Image
+                  src="/assets/icons/appleIcon.png"
+                  width={15}
+                  height={15}
+                  alt="icon"
+                />
+              </div>
+              apple
+            </button>
+          </div>
+        </form>
+      </LoginContainer>
+      <Toast toastState={toastState} setToastState={setToastState} />
+    </Fragment>
   );
 };
 
