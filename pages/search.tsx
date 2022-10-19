f38feb8106f5d3f1c6f31c6c9cf9f2703624ce45
Siddhant-ops <siddhantdalvi3@gmail.com>
2022-10-19 10:15:41 +0530

    mapbox implementation done

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,9 +1,36 @@
 import { NextPage } from 'next';
 import CardTypeOne from '../Components/Card/CardTypeOne';
 import Layout from '../Components/Layout/Layout';
+import ReactMapGL, {
+  AttributionControl,
+  GeolocateControl,
+  Marker,
+  NavigationControl
+} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import searchStyles from '../styles/Search/search.module.scss';
+import { useEffect, useState } from 'react';
 
 const ExploreHome: NextPage = () => {
+  const [viewportState, setViewportState] = useState({
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 11
+  });
+
+  useEffect(() => {
+    //  get location from browser
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position.coords);
+
+      setViewportState({
+        ...viewportState,
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      });
+    });
+  }, []);
+
   return (
     <Layout>
       <div className={searchStyles.filterSection}></div>
@@ -15,7 +42,24 @@ const ExploreHome: NextPage = () => {
               <CardTypeOne key={index} />
             ))}
         </div>
-        <div className={searchStyles.mapSection}></div>
+        <div className={searchStyles.mapSection}>
+          <ReactMapGL
+            style={{ width: '100%', height: '100%' }}
+            mapStyle="mapbox://styles/shobhit24/ckopb9huq8vwt18qvw4gxh271"
+            // // {...viewportState}
+            mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN ?? ''}
+            // mapStyle="mapbox://styles/mapbox/ckszotu58a7dz17qh8ysv970j"
+            onMove={(e) => setViewportState(e.viewState)}
+            latitude={viewportState.latitude}
+            longitude={viewportState.longitude}
+            zoom={viewportState.zoom}
+            attributionControl={false}
+          >
+            <GeolocateControl showAccuracyCircle />
+            <AttributionControl customAttribution="Albergo" />
+            <NavigationControl />
+          </ReactMapGL>
+        </div>
       </div>
     </Layout>
   );
