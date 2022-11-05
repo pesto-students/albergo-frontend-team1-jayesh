8e0191af55864ae05652c4f067cfb87ace2ffd39
Siddhant-ops <siddhantdalvi3@gmail.com>
2022-11-05 09:55:18 +0530

    changes

--- pages/api/hotel/search.ts
+++ pages/api/hotel/search.ts
@@ -23,28 +23,31 @@ export default function handler(
       searchBy: req.body
     });
 
-    await fetch(`${NEXT_PUBLIC_API_URL}/hotel/search`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: raw
-    })
-      .then((response) => {
-        if (!response.ok) {
-          res.status(200).json({ data: [] });
-          return;
-        }
-        return response.json()
-      })
-      .then((response) => {
-        res.status(200).json({ data: response.data });
-        resolve();
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(400).json({ error: 'Please try again later' });
-        resolve();
+    try {
+      const response = await fetch(`${NEXT_PUBLIC_API_URL}/hotel/search`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: raw
       });
+
+      if (!response.ok) {
+        res.status(400).json({
+          data: []
+        });
+        resolve()
+      }
+
+      if (response.ok) {
+        const responseData = await response.json();
+
+        res.status(200).json({ data: responseData.data })
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ error: 'Please try again later' });
+      resolve();
+    }
   });
 }
